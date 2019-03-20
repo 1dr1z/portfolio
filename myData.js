fd272f3d19de8ea91e69a90e121c52bb011498ca
1dr1z <idriz.96@hotmail.com>
2019-03-20 22:54:27 +0100

    initial commit

---
+++ myData.js
@@ -0,0 +1,257 @@
+export const cover = {
+  id: 1,
+  title: "Cover",
+  img: "img/cover.jpg"
+};
+
+export const profile = {
+  id: 1,
+  title: "Profile",
+  img: "img/profile.png"
+};
+export const basicInfo = {
+  id: 1,
+  fullName: "Idriz Mehmedovic",
+  address: "Kamenica Donja, 164",
+  ZIP: "75410",
+  city: "Drinjaca",
+  country: "BiH",
+  phoneNo: "+387 62-263-195",
+  email: "idriz.mhm@gmail.com"
+};
+
+export const education = [
+  {
+    id: 1,
+    city: "Tuzla",
+    school: "Behram-begova medresa",
+    dateRange: "2010-2014",
+    description:
+      "Behram-bey's medresa is a educational institution that provides knowledge in various areas: Islamic, natural-mathematical, language, social and multidisciplinary, as well as electoral teaching."
+  },
+  {
+    id: 2,
+    city: "Tuzla",
+    school: "IPI Academy",
+    dateRange: "2017-ongoing",
+    description:
+      "I attend a study program of Information Technology where we are training to define and solve complex engineering problems as well as communication skills and skills of cooperating and working in teams."
+  }
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    position: "Vice President",
+    organization: 'NGO "Studentski Klub IPI Akademije"',
+    dateRange: "2018-ongoing",
+    description:
+      "On December 18th I was elected as Vice President of the NGO “Studentski klub IPI Akademije”. My responsibilities include: Authorization of the President, coordinate and guide the work of individual sections of the Association, planning and coordination of student activities and excursions, developing a thriving environment for all students at the Academy.",
+    link: ""
+  },
+  {
+    id: 2,
+    position: "Volunteer",
+    organization: "BH Engineering Week Tuzla",
+    dateRange: "2018",
+    description:
+      "On November 28th and 29th during the BH Engineering Week conference in Tuzla I was volunteering as Technical support.",
+    link: ""
+  },
+  {
+    id: 3,
+    position: "Marketing",
+    organization: "---",
+    dateRange: "2015-2016",
+    description:
+      "Digital Marketing Strategy, SEO optimization, Content Creation/Management.",
+    link: ""
+  },
+  {
+    id: 4,
+    position: "Web Application Penetration Testing",
+    organization: "---",
+    dateRange: "2015-2016",
+    description:
+      "As a web application penetration tester, I preferred to use the Kali Linux operating system and its built-in tools. I would like to point out some of them, which are BurpSuite, Wfuzz, WPScan and Metasploit framework",
+    link: ""
+  },
+  {
+    id: 5,
+    position: "Maintenance of Computers",
+    organization: "---",
+    dateRange: "2014-ongoing",
+    description:
+      "Computer testing, computer analysis, upgrading computer software and hardware, Installing operating systems(Windows, Windows Server, FreeBSD, Linux)",
+    link: ""
+  }
+];
+
+export const projects = [
+  {
+    id: 1,
+    type: "Student Project",
+    title: "Student Service Simulator",
+    dateRange: "2018",
+    description: "",
+    link: ""
+  },
+  {
+    id: 2,
+    type: "Wordpress Website",
+    title: "NK Jedinstvo Ljubnici Ilijas",
+    dateRange: "2017",
+    description:
+      "A project that lasted two months. My responsibilities were to create a website for the mentioned club, perform all necessary hosting settings, backup, mail server etc.",
+    link: "http://nkjedinstvoljubnici.ba/"
+  },
+  {
+    id: 3,
+    type: "Content Management System",
+    title: "---",
+    dateRange: "2017",
+    description:
+      "Guided by the desire to improve my knowledge in the development of web applications, I started learning PHP. Through the tutorial I created a content management system for the blog. The technologies I used for the frontend were HTML, CSS (Bootstrap), and for backend PHP and MySQL.",
+    link: "http://nkjedinstvoljubnici.ba/"
+  }
+];
+
+export const competitions = [
+  {
+    id: 1,
+    competition: "SBF Start-up Challenge",
+    dateRange: "2019",
+    description:
+      "SBF Start-up Challenge is a competition of (university) teams and business ideas evaluated by an expert jury. It is a competition of knowledge through which efforts to popularize entrepreneurship and promote positive values. The purpose of the SBF Start-up Challenge project is to raise awareness about the importance of entrepreneurship and motivate young people in Bosnia and Herzegovina to engage in entrepreneurial activites.",
+    link: "https://www.youtube.com/watch?v=LLWi0iycpPs"
+  },
+  {
+    id: 2,
+    competition: "MOL FRESHHH",
+    dateRange: "2018",
+    description:
+      "The competition started on November 21st and lasted for 10 days. Freshhh is an online international student competition, where students in teams of 3 members have to take over the management of an integrated oil corporation. Make decisions on field development projects, build refinery for different products and find the best product portfolio, and set strategy on gas station network where aim is to make profit by expanding, investment network portfolio.",
+    link: "https://freshhh.net/"
+  }
+];
+
+export const langAndTechs = [
+  {
+    id: 9,
+    alt: "REACT",
+    img: "img/react.svg"
+  },
+  {
+    id: 1,
+    alt: "HTML",
+    img: "img/html.png"
+  },
+  {
+    id: 2,
+    alt: "CSS",
+    img: "img/css.png"
+  },
+  {
+    id: 3,
+    alt: "JS",
+    img: "img/js.png"
+  },
+  {
+    id: 4,
+    alt: "JAVA",
+    img: "img/java.png"
+  },
+  {
+    id: 5,
+    alt: "C++",
+    img: "img/cpp.png"
+  },
+  {
+    id: 6,
+    alt: "C",
+    img: "img/c.png"
+  },
+  {
+    id: 7,
+    alt: "LINUX",
+    img: "img/linux.png"
+  },
+  {
+    id: 8,
+    alt: "WINDOWS",
+    img: "img/win.png"
+  },
+  {
+    id: 10,
+    alt: "WINSRVR",
+    img: "img/winsrvr.png"
+  },
+  {
+    id: 11,
+    alt: "MYSQL",
+    img: "img/mysql.png"
+  },
+  {
+    id: 12,
+    alt: "MSVISUALSTUDIO",
+    img: "img/vs.png"
+  },
+  {
+    id: 13,
+    alt: "CODEBLOCKS",
+    img: "img/cb.png"
+  },
+  {
+    id: 14,
+    alt: "ECLIPSE",
+    img: "img/ecl.png"
+  },
+  {
+    id: 15,
+    alt: "BRACKETS",
+    img: "img/brack.png"
+  },
+  {
+    id: 16,
+    alt: "ATOM",
+    img: "img/atom.png"
+  },
+  {
+    id: 17,
+    alt: "HYPER-V",
+    img: "img/hyper.png"
+  },
+  {
+    id: 18,
+    alt: "VMWARE",
+    img: "img/vmware.png"
+  },
+  {
+    id: 18,
+    alt: "PS",
+    img: "img/ps.png"
+  }
+];
+
+export const languages = [
+  {
+    id: 1,
+    understanding: "B2",
+    speaking: "B2",
+    writing: "B2"
+  }
+];
+
+export const commSkills = [
+  "Goal oriented professional with excellent interpersonal and communication skills",
+  "Self-starter, able to work well with minimal directions",
+  "Interact supportively within a team environment",
+  "Maintain excellent written and oral communication skills, problem resolution abilities, and a high level of confidentiality"
+];
+
+export const managerialSkills = [
+  "Highly organized, accomplish assigned tasks in an efficient manner",
+  "Work well under pressure within a fast-paced environment",
+  "Fast learner, can grasp and apply new procedures quickly",
+  "Young, honest, organized, conscientious, independent, hard-working, friendly, confident, polite, responsible, goal-oriented, energetic, willing to learn."
+];
